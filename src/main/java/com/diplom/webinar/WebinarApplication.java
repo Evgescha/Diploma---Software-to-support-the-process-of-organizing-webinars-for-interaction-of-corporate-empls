67da40efa61ea3a34371ca698917afffed4f99d9
yauhescha <evgescha.maslov007@yandex.ru>
2020-12-01 14:38:31 +0300

    Создан пустой проект

---
+++ src/main/java/com/diplom/webinar/WebinarApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.webinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebinarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplom/webinar/WebinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.webinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
